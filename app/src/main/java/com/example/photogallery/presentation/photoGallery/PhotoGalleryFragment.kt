03a06b0173eb0e6f4ac67980eae51afee4dfb762
OnicaAdrianCatalin <102966412+OnicaAdrianCatalin@users.noreply.github.com>
2022-06-20 14:41:05 +0300

    Implemented Glide library for downloading the images

--- app/src/main/java/com/example/photogallery/presentation/photoGallery/PhotoGalleryFragment.kt
+++ app/src/main/java/com/example/photogallery/presentation/photoGallery/PhotoGalleryFragment.kt
@@ -1,16 +1,12 @@
 package com.example.photogallery.presentation.photoGallery
 
-import android.graphics.drawable.BitmapDrawable
 import android.os.Bundle
-import android.os.Handler
-import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
-import com.example.photogallery.ThumbnailDownloader
 import com.example.photogallery.databinding.FragmentPhotoGalleryBinding
 
 class PhotoGalleryFragment : Fragment() {
@@ -18,17 +14,6 @@ class PhotoGalleryFragment : Fragment() {
     private lateinit var binding: FragmentPhotoGalleryBinding
     private val viewModel: PhotoGalleryViewModel by lazy {
         ViewModelProvider(this).get(PhotoGalleryViewModel::class.java)
-    }
-    private lateinit var thumbnailDownloader: ThumbnailDownloader<PhotoAdapter.PhotoHolder>
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        val responseHandler = Handler(Looper.getMainLooper())
-        thumbnailDownloader = ThumbnailDownloader(responseHandler) { photoHolder, bitmap ->
-            val drawable = BitmapDrawable(resources, bitmap)
-            photoHolder.bindDrawable(drawable)
-        }
-        lifecycle.addObserver(thumbnailDownloader.fragmentLifecycleObserver)
     }
 
     override fun onCreateView(
@@ -44,7 +29,6 @@ class PhotoGalleryFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
         observeData()
-        viewLifecycleOwner.lifecycle.addObserver(thumbnailDownloader.viewLifecycleObserver)
     }
 
     private fun observeData() {
@@ -52,18 +36,8 @@ class PhotoGalleryFragment : Fragment() {
             viewLifecycleOwner
         ) { galleryItems ->
             binding.photoRecyclerView.adapter =
-                PhotoAdapter(galleryItems, requireContext(), thumbnailDownloader)
+                PhotoAdapter(galleryItems, requireContext())
         }
-    }
-
-    override fun onDestroy() {
-        super.onDestroy()
-        viewLifecycleOwner.lifecycle.removeObserver(
-            thumbnailDownloader.viewLifecycleObserver
-        )
-        lifecycle.removeObserver(
-            thumbnailDownloader.fragmentLifecycleObserver
-        )
     }
 
     companion object {
