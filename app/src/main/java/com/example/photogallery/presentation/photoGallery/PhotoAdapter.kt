5df5c675b00ce2b3102957e71c00478f8a02655c
Adrian Onica <>
2022-07-13 15:14:52 +0300

    Made items clickable and added PhotoPageActivity and PhotoPageFragment to navigate in WebView

--- app/src/main/java/com/example/photogallery/presentation/photoGallery/PhotoAdapter.kt
+++ app/src/main/java/com/example/photogallery/presentation/photoGallery/PhotoAdapter.kt
@@ -2,10 +2,12 @@ package com.example.photogallery.presentation.photoGallery
 
 import android.content.Context
 import android.view.LayoutInflater
+import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
+import com.example.photogallery.PhotoPageActivity
 import com.example.photogallery.R
 import com.example.photogallery.data.model.GalleryItem
 
@@ -15,19 +17,36 @@ class PhotoAdapter(
 ) :
     RecyclerView.Adapter<PhotoAdapter.PhotoHolder>() {
 
-    class PhotoHolder(itemImageView: ImageView) :
-        RecyclerView.ViewHolder(itemImageView)
+    class PhotoHolder(itemImageView: ImageView, val context: Context) :
+        RecyclerView.ViewHolder(itemImageView),
+        View.OnClickListener {
+        private lateinit var galleryItem: GalleryItem
+
+        init {
+            itemView.setOnClickListener(this)
+        }
+
+        fun bindGalleryItem(item: GalleryItem) {
+            galleryItem = item
+            Glide.with(context).load(galleryItem.url).placeholder(R.drawable.hide_image)
+                .into(itemView as ImageView)
+        }
+
+        override fun onClick(view: View) {
+            val intent = PhotoPageActivity.newIntent(context, galleryItem.photoPageUri)
+            view.context.startActivity(intent)
+        }
+    }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PhotoHolder {
         val layoutInflater = LayoutInflater.from(parent.context)
         val view = layoutInflater.inflate(R.layout.list_item_gallery, parent, false) as ImageView
-        return PhotoHolder(view)
+        return PhotoHolder(view, context)
     }
 
     override fun onBindViewHolder(holder: PhotoHolder, position: Int) {
         val galleryItem = galleryItem[position]
-        Glide.with(context).load(galleryItem.url).placeholder(R.drawable.hide_image)
-            .into(holder.itemView as ImageView)
+        holder.bindGalleryItem(galleryItem)
     }
 
     override fun getItemCount(): Int = galleryItem.size
